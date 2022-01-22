189ffbc7309e889e7e34f27dc505762f8bace8ec
agniAZ <agni_zlataric@hotmail.com>
2022-01-22 19:27:05 +0100

    chore: styling update

--- templates/about.js
+++ templates/about.js
@@ -1,9 +1,9 @@
 let about = `
-  <section class="about">
-    o radu
-
+  <section class="about col-12">
+      <div class="row  ml-0 mr-0">
+        o radu
+      </div>
   </section>
-
 `;
 
 
--- templates/stylelanguages.js
+++ templates/stylelanguages.js
@@ -1,10 +1,10 @@
 let stylelanguages = `
   <section class="stylelanguages col-12">
     <div class="row ml-0 mr-0">
-      <div class="col-sm-6">
+      <div class="col-sm-6 p-0">
        Bootstrap
        </div>
-      <div class="col-sm-6">
+      <div class="col-sm-6 p-0">
       Bulma
         </div>
     </div>
